06989b35772b3bc4b3af163d55d15f2e1ad00c00
Tianshuo Deng <tianshuo@macbook-pro.lan>
2019-06-21 16:19:05 -0700

    first commit

---
+++ bash_aliases
@@ -0,0 +1,39 @@
+# -*- mode: sh -*-
+
+# only use aliases that is cross platform between linux and BSD.
+
+# Emacs
+export EDITOR="emacs -nw"
+
+# Colorful terminal
+export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ "
+export CLICOLOR=1
+export LSCOLORS=GxFxCxDxBxegedabagaced
+
+# Better ls
+# -G enable colorized output
+# -F add readable suffix / for dir, * for executable, @ for link, etc.
+# -h human readable size unit.
+alias ls='ls -GFh'
+alias ll='ls -GFlah'
+
+# grep with color
+alias grep='grep --color=auto'
+
+# ssh with color
+alias ssh='TERM=xterm-256color ssh'
+
+# go_earth
+alias go_earth='ssh -t tsdeng@dengtianshuo.com -p 2323'
+
+# git shortcuts
+alias gst='git status'
+alias gco='git checkout'
+alias glg='git lg'
+alias gci='git commit'
+alias gad='git add'
+alias gbr='git branch'
+alias gps='git push'
+alias gpl='git pull'
+alias gmg='git merge'
+alias gdf='git diff'
---
+++ install.sh
@@ -0,0 +1,39 @@
+if [ -f ~/.bash_aliases ]; then
+    echo "~/.bash_aliases already exists, will do nothing."
+    exit
+fi
+ln -Fs $(pwd)/bash_aliases ~/.bash_aliases
+
+function insert_to_file() {
+    CONTENT=$1
+    TARGET_FILE=$2
+    # -F means fixed string, -x means exactly match whole line. -q means quiet.
+    if ! grep -Fxq "$CONTENT" $TARGET_FILE; then
+        echo "updating $TARGET_FILE with: $CONTENT"
+        # using printf here because BSD's echo does not recognize \n.
+        # https://lists.freebsd.org/pipermail/freebsd-questions/2011-December/236645.html
+        printf "$CONTENT\n$(cat $TARGET_FILE)" > $TARGET_FILE
+    fi
+}
+
+
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # In OSX all shells are logins shells and they all read ~/.bash_profile.
+    echo "Your system is OSX, updating ~/.bash_profile to source ~/.bash_aliases"
+    insert_to_file 'source ~/.bash_aliases' ~/.bash_profile
+
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+    # In linux, the recommendation is to put setting in ~/.bashrc(read for
+    # non-login shells) and then let ~/.bash_profile(for login shells)
+    # source ~/.bashrc.
+    echo "Your system is linux-gnu, updating ~/.bashrc to source ~/.bash_aliases."
+    insert_to_file 'source ~/.bash_aliases' ~/.bashrc
+fi
+
+
+
+if [[ "$OSTYPE" == "d"* ]]; then
+    echo "Your system is OSX, updating ~/.bash_profile to source ~/.bash_aliases"
+    insert_to_file 'source ~/.bash_aliases' ~/.bash_profile
+fi
